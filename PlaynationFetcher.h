fb6cc1795c0e92698d9b7913a33b883ac05409b5
Lasse Wingreen <lassewingreen@gmail.com>
2014-02-17 01:07:22 +0100

    lolz

---
+++ PlaynationFetcher.h
@@ -0,0 +1,13 @@
+//
+//  PlaynationFetcher.h
+//  Playnation App V 0.3
+//
+//  Created by Lasse Wingreen on 04/02/14.
+//  Copyright (c) 2014 Remus Cicu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlaynationFetcher : NSObject
+
+@end
---
+++ PlaynationFetcher.m
@@ -0,0 +1,13 @@
+//
+//  PlaynationFetcher.m
+//  Playnation App V 0.3
+//
+//  Created by Lasse Wingreen on 04/02/14.
+//  Copyright (c) 2014 Remus Cicu. All rights reserved.
+//
+
+#import "PlaynationFetcher.h"
+
+@implementation PlaynationFetcher
+
+@end
